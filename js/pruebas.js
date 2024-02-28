5d8c148c7232a6620f5a155252cc6c3d9ba27847
Nicolas Sassone <nico_distortion@hotmail.com>
2024-02-28 15:13:48 -0300

    subir prueba

---
+++ js/pruebas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var paleta = document.getElementById('colorGrid'); // Selecciona el contenedor de la paleta de colores
+  var texto = document.getElementById('texto'); // Selecciona el contenedor de texto
+
+  // Crear matriz de colores RGB
+  for (var r = 0; r < 256; r += 16) {
+      for (var g = 0; g < 256; g += 16) {
+          for (var b = 0; b < 256; b += 16) {
+              // Crear elemento div para cada color
+              var colorDiv = document.createElement('div');
+              colorDiv.className = 'color';
+              colorDiv.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+              // Agregar evento click para cambiar el color del texto
+              colorDiv.addEventListener('click', function() {
+                  var color = this.style.backgroundColor;
+                  texto.style.color = color; // Cambia el color del texto al color seleccionado
+              });
+
+              // Agregar elemento a la paleta
+              paleta.appendChild(colorDiv); // Agrega el color a la paleta de colores
+          }
+      }
+  }
+});
