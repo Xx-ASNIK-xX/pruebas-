939b9c7b74b978ab4d97eecd83f1ff7c99aa4a28
Nicolas Sassone <nico_distortion@hotmail.com>
2024-02-28 17:12:25 -0300

    ultimos cambios

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,25 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var paleta = document.getElementById('colorGrid'); // Selecciona el contenedor de la paleta de colores
-  var texto = document.getElementById('texto'); // Selecciona el contenedor de texto
+    var colorGrid = document.getElementById('colorGrid');
+    var selectedColor = document.getElementById('selectedColor');
 
-  // Crear matriz de colores RGB
-  for (var r = 0; r < 256; r += 16) {
-      for (var g = 0; g < 256; g += 16) {
-          for (var b = 0; b < 256; b += 16) {
-              // Crear elemento div para cada color
-              var colorDiv = document.createElement('div');
-              colorDiv.className = 'color';
-              colorDiv.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    // Generar la cuadrícula de colores RGB
+    for (var r = 0; r <= 255; r += 16) {
+        for (var g = 0; g <= 255; g += 16) {
+            for (var b = 0; b <= 255; b += 16) {
+                var colorDiv = document.createElement('div');
+                colorDiv.className = 'color';
+                colorDiv.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+                
+                // Agregar evento de clic para mostrar el color seleccionado
+                colorDiv.addEventListener('click', function() {
+                    var color = this.style.backgroundColor;
+                    selectedColor.textContent = 'Color seleccionado: ' + color;
+                });
 
-              // Agregar evento click para cambiar el color del texto
-              colorDiv.addEventListener('click', function() {
-                  var color = this.style.backgroundColor;
-                  texto.style.color = color; // Cambia el color del texto al color seleccionado
-              });
-
-              // Agregar elemento a la paleta
-              paleta.appendChild(colorDiv); // Agrega el color a la paleta de colores
-          }
-      }
-  }
+                colorGrid.appendChild(colorDiv);
+            }
+        }
+    }
 });
